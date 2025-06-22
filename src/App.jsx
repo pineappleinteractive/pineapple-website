815f6f04f017a1571b2a2c632a28f5485d76fb9e
Jason Fell <fell.jason.s@gmail.com>
2025-06-21 17:06:06 -0700

    first website build

---
+++ src/App.jsx
@@ -0,0 +1,27 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import PILogo from './assets/pin-int-logo-v03a.png';
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <img src={PILogo} alt="Pineapple Interactive logo" width={'75%'} />
+      {/* <img src={PILogo} className="logo" alt="Pineapple Interactive logo" /> */}
+      {/* <h1>Pineapple Interactive</h1> */}
+      <div className="card">
+        <p>
+          We build web apps and video games!
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Come back for more!
+      </p>
+    </>
+  )
+}
+
+export default App
