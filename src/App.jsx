76e690be4e946f492d1eb36faf42724b0077872e
Jason Fell <fell.jason.s@gmail.com>
2025-06-24 20:55:34 -0700

    Updating again.

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
-import PILogo from '/favicon.png';
+import PILogo from './assets/pin-int-logo-v03a.png';
 import './App.css'
 
 function App() {
