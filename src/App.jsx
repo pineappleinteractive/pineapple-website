ae706bf0889094c4c92bdb042daa1b9273fe067a
Jason Fell <fell.jason.s@gmail.com>
2025-06-26 12:38:24 -0700

    removing custom css classes from app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -7,12 +7,12 @@ function App() {
   return (
     <div>
       <img src={logo} alt="Pineapple Interactive logo" width={'65%'} />
-      <div className="card">
-        <p>
-          We build web apps and video games!
-        </p>
-      </div>
-      <p className="read-the-docs">
+      {/* <div className="card"> */}
+        <p>We build web apps and video games!</p>
+      {/* <p className="read-the-docs"></p> */}
+      <p style={{
+        color: '#888',
+        }}>
         Come back for more!
       </p>
     </div>
