7b9c8ae9508d782f72176c26d6ae46f6f67d1455
Jason Fell <fell.jason.s@gmail.com>
2025-06-26 12:42:29 -0700

    reverting changes too app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -5,25 +5,25 @@ import './App.css'
 function App() {
 
   return (
-    <div>
-      <img src={logo} alt="Pineapple Interactive logo" width={'65%'} />
-      {/* <div className="card"> */}
-        <p>We build web apps and video games!</p>
-      {/* <p className="read-the-docs"></p> */}
-      <p style={{
-        color: '#888',
-        }}>
-        Come back for more!
-      </p>
-    </div>
-    // <div style={{
-    //   backgroundColor: 'black',
-    //   color: 'lime',
-    //   padding: '2rem',
-    //   fontSize: '2rem'
-    // }}>
-    //   ✅ Pineapple Interactive is LIVE!
+    // <div>
+    //   <img src={logo} alt="Pineapple Interactive logo" width={'65%'} />
+    //   {/* <div className="card"> */}
+    //     <p>We build web apps and video games!</p>
+    //   {/* <p className="read-the-docs"></p> */}
+    //   <p style={{
+    //     color: '#888',
+    //     }}>
+    //     Come back for more!
+    //   </p>
     // </div>
+    <div style={{
+      backgroundColor: 'black',
+      color: 'lime',
+      padding: '2rem',
+      fontSize: '2rem'
+    }}>
+      ✅ Pineapple Interactive is LIVE!
+    </div>
   )
 }
 
