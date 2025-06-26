b5f2faa482430b0954b9cbb039faf152a4c6e16a
Jason Fell <fell.jason.s@gmail.com>
2025-06-26 12:21:19 -0700

    fixing syntax

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,12 @@
 import { useState } from 'react'
 import logo from './assets/plogo.png';
-import './App.css'
+import './App.css';
 
 function App() {
 
   return (
     <>
-      {/* <img src={logo} alt="Pineapple Interactive logo" width={'75%'} /> */}
-      {/* <img src={PILogo} className="logo" alt="Pineapple Interactive logo" /> */}
-      {/* <h1>Pineapple Interactive</h1> */}
+      <img src={logo} alt="Pineapple Interactive logo" width={'65%'} />
       <div className="card">
         <p>
           We build web apps and video games!
